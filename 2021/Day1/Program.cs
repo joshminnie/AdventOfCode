9a19736b7cd56b9300062468989247d35b146764
Josh Minnie <josh.minnie@gmail.com>
2021-12-02 16:46:32 -0500

    2021: Day 1 & Day 2

---
+++ 2021/Day1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace AoC2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"input.txt");
+            int[] numbers = Array.ConvertAll<string, int>(lines, int.Parse);
+
+            Console.WriteLine("{0} single step increase found.", SingleStepIncreases(lines));
+
+            Console.WriteLine("{0} windowed increases found.", WindowedIncreases(numbers));
+        }
+
+        static int SingleStepIncreases(string[] lines)
+        {
+            int increases = 0;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var previous = Convert.ToInt64(lines[i - 1]);
+                var current = Convert.ToInt64(lines[i]);
+
+                if (current > previous)
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+
+        static int WindowedIncreases(int[] numbers)
+        {
+            int increases = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (i + 2 >= numbers.Length)
+                {
+                    break;
+                }
+
+                var previous = numbers[i - 1] + numbers[i] + numbers[i + 1];
+                var current = numbers[i] + numbers[i + 1] + numbers[i + 2];
+
+                if (current > previous)
+                {
+                    increases++;
+                }
+            }
+
+            return increases;
+        }
+    }
+}
---
+++ 2021/Day2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"input.txt");
+            var directions = lines.Select(p => p.Split(" ")).ToArray();
+
+            Console.WriteLine("{0} is the final position.", CalculatePosition(directions));
+
+            Console.WriteLine("{0} is the angled position.", CalculateAngledPosition(directions));
+        }
+
+        static int CalculatePosition(string[][] directions)
+        {
+            int depth = 0, distance = 0;
+            for (int i = 0; i < directions.Length; i++)
+            {
+                switch(directions[i][0])
+                {
+                    case "forward":
+                        distance += int.Parse(directions[i][1]);
+                        break;
+                    case "up":
+                        depth -= int.Parse(directions[i][1]);
+                        break;
+                    case "down":
+                        depth += int.Parse(directions[i][1]);
+                        break;
+                }
+            }
+
+            return depth * distance;
+        }
+
+        static int CalculateAngledPosition(string[][] directions)
+        {
+            int depth = 0, angle = 0, distance = 0, movement;
+            for (int i = 0; i < directions.Length; i++)
+            {
+                movement = int.Parse(directions[i][1]);
+                switch (directions[i][0])
+                {
+                    case "forward":
+                        distance += movement;
+                        depth += angle * movement;
+                        break;
+                    case "up":
+                        angle -= movement;
+                        break;
+                    case "down":
+                        angle += movement;
+                        break;
+                }
+            }
+
+            return depth * distance;
+        }
+    }
+}
