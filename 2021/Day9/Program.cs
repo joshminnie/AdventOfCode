ba840da3bc2a8bc9076c83c7c313e3d6b8866996
Josh Minnie <josh.minnie@gmail.com>
2021-12-09 21:02:35 -0500

    Add Day 9 / Part 1 solution

---
+++ 2021/Day9/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using AdventOfCode;
+using Day9.Support;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day9Part1Solution().CalculateSolution();
+            new Day9Part2Solution().CalculateSolution();
+        }
+    }
+
+    public abstract class Day9SolutionBase : SolutionBase
+    {
+        public int[][] Values {
+            get
+            {
+                return Lines.Select(p => p.ToCharArray().Select(v => int.Parse(v.ToString())).ToArray()).ToArray();
+            }
+        }
+    }
+
+
+    [DisplayName("Day 9 / Part 1 Solution")]
+    public class Day9Part1Solution : Day9SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            var lowPoints = new List<Coordinate>();
+
+            for (int x = 0; x < Values.Length; x++)
+            {
+                for (int y = 0; y < Values[x].Length; y++)
+                {
+                    if (Values[x][y] == 9)
+                        continue;
+
+                    var origin = new Coordinate(x, y, Values[x][y]);
+                    var adjacents = origin.FindAdjacents(Values);
+
+                    if (adjacents.All(a => a.Value > origin.Value))
+                    {
+                        lowPoints.Add(origin);
+                    }
+                }
+            }
+
+            RenderResult("Risk level", lowPoints.Sum(p => p.Value + 1));
+        }
+    }
+
+    [DisplayName("Day 9 / Part 2 Solution")]
+    public class Day9Part2Solution : Day9SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            var basins = new List<List<Coordinate>>();
+
+            for (int x = 0; x < Values.Length; x++)
+            {
+                for (int y = 0; y < Values[x].Length; y++)
+                {
+                    if (Values[x][y] == 9)
+                        continue;
+
+                    var origin = new Coordinate(x, y, Values[x][y]);
+                    var adjacents = origin.FindAdjacents(Values);
+                }
+            }
+        }
+    }
+}
---
+++ 2021/Day9/Support/Coordinate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Day9.Support
+{
+    public class Coordinate
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Value { get; }
+
+        public Coordinate(int x, int y, int value)
+        {
+            X = x;
+            Y = y;
+            Value = value;
+        }
+
+        public List<Coordinate> FindAdjacents(int[][] values)
+        {
+            var adjacents = new List<Coordinate>
+            {
+                GetCoordinate(values, X - 1, Y), // top
+                GetCoordinate(values, X + 1, Y), // bottom
+                GetCoordinate(values, X, Y - 1), // left
+                GetCoordinate(values, X, Y + 1) // right
+            };
+
+            adjacents.RemoveAll(x => x == null);
+
+            return adjacents;
+        }
+
+        private Coordinate GetCoordinate(int[][] values, int x, int y)
+        {
+            try
+            {
+                return new Coordinate(x, y, values[x][y]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return null;
+            }
+        }
+    }
+}
