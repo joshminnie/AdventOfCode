ab2537f2c4a07cfcd04cf5cb4c13c670790bc272
Josh Minnie <josh.minnie@gmail.com>
2021-12-15 18:07:00 -0500

    Add Day 5 assets and beginning of solution

---
+++ 2021/Day5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using AdventOfCode;
+using Day5.Support;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day5Part1Solution().CalculateSolution();
+            new Day5Part2Solution().CalculateSolution();
+        }
+    }
+
+    public class Day5Part1Solution : SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            var lines = Lines;
+            var vents = new List<Point>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var points = lines[i].Trim().Split(" -> ").Select((string s) =>
+                {
+                    var points = Array.ConvertAll(s.Split(","), int.Parse);
+                    return new Point(points[0], points[1]);
+                }).ToList();
+
+                var line = new Line(points[0], points[1]);
+                vents.AddRange(line.GetAllPoints());
+            }
+
+            var overlaps = from x in vents
+                           group x by x into g
+                           let count = g.Count()
+                           orderby count descending
+                           where count > 1
+                           select new { Value = g.Key, Count = count };
+
+            RenderResult("Part 1", null);
+        }
+    }
+
+    public class Day5Part2Solution : SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            RenderResult("Part 1", null);
+        }
+    }
+}
---
+++ 2021/Day5/Support/Line.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Day5.Support
+{
+    public class Line
+    {
+        public Point StartingPoint
+        {
+            get;
+            private set;
+        }
+
+        public Point EndingPoint
+        {
+            get;
+            private set;
+        }
+
+        public bool IsHorizontal
+        {
+            get
+            {
+                return StartingPoint.X == EndingPoint.X;
+            }
+        }
+
+        public bool IsVertical
+        {
+            get
+            {
+                return StartingPoint.Y == EndingPoint.Y;
+            }
+        }
+
+        public Line(Point p1, Point p2)
+        {
+            var points = new List<Point> { p1, p2 };
+            points.Sort(new PointComparer());
+
+            StartingPoint = points.First();
+            EndingPoint = points.Last();
+        }
+
+        public List<Point> GetAllPoints()
+        {
+            var points = new List<Point> { StartingPoint, EndingPoint };
+
+            if (IsHorizontal)
+            {
+                for (int i = StartingPoint.Y + 1; i < EndingPoint.Y; i++)
+                {
+                    points.Add(new Point(StartingPoint.X, i));
+                }
+            }
+            else if (IsVertical)
+            {
+                for (int i = StartingPoint.X + 1; i < EndingPoint.X; i++)
+                {
+                    points.Add(new Point(i, StartingPoint.Y));
+                }
+            }
+
+            return points;
+        }
+    }
+
+    public class PointComparer : IComparer<Point>
+    {
+        public int Compare(Point a, Point b)
+        {
+            if (a.X.CompareTo(b.X) != 0)
+            {
+                return a.X.CompareTo(b.X);
+            }
+            else if (a.Y.CompareTo(b.Y) != 0)
+            {
+                return a.Y.CompareTo(b.Y);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
