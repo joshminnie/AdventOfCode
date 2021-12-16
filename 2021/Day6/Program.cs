e4e59a83b8751b6875fd5a1021159744e199d15c
Josh Minnie <josh.minnie@gmail.com>
2021-12-16 17:02:51 -0500

    Add Day 6 solution project and assets

---
+++ 2021/Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using AdventOfCode;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day6Part1Solution().CalculateSolution();
+            new Day6Part2Solution().CalculateSolution();
+        }
+    }
+
+    [DisplayName("Day 6/Part 1 Solution")]
+    public class Day6Part1Solution : SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [DisplayName("Day 6/Part 2 Solution")]
+    public class Day6Part2Solution : SolutionBase
+    {
+        public override void CalculateSolution()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
