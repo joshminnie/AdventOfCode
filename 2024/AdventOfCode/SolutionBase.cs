230c3e5ae9c33a3beb229a2f407eba1e74f7460a
Josh Minnie <josh.minnie@gmail.com>
2024-12-05 13:57:03 -0700

    Add solution for day 1

---
+++ 2024/AdventOfCode/SolutionBase.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace AdventOfCode;
+
+public abstract class SolutionBase
+{
+    /// <summary>
+    /// Gets the input for the given Advent of Code problem.
+    /// </summary>
+    protected static string Input => @"input.txt";
+
+    /// <summary>
+    /// Gets the human-readable display name for the class. If a <see cref="DisplayNameAttribute"/> is provided, returns the name; otherwise,
+    /// the raw class name.
+    /// </summary>
+    private string DisplayName
+    {
+        get
+        {
+            var attribute = (DisplayNameAttribute?)Attribute.GetCustomAttribute(this.GetType(), typeof(DisplayNameAttribute), true);
+
+            return attribute == null ? GetType().Name : attribute.DisplayName;
+        }
+    }
+
+    /// <summary>
+    /// Reads the file contents into the appropriate data storage object.
+    /// </summary>
+    public abstract void ReadFile();
+
+    /// <summary>
+    /// Calculates the solution for the given Advent of Code problem.
+    /// </summary>
+    public abstract void CalculateSolution();
+
+    /// <summary>
+    /// Renders the solution to the <see cref="Console"/>.
+    /// </summary>
+    /// <param name="result"></param>
+    public virtual void RenderResult(object result)
+    {
+        RenderResult(null, result);
+    }
+
+    protected virtual void RenderResult(string? message, object result)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(string.IsNullOrEmpty(message) ? $"{this.DisplayName}: " : $"{this.DisplayName} | {message}: ");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(result);
+
+        Console.ResetColor();
+    }
+}
---
+++ 2024/Day01/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using AdventOfCode;
+using Microsoft.VisualBasic.FileIO;
+
+new Day1Part1Solution().CalculateSolution();
+new Day1Part2Solution().CalculateSolution();
+
+internal abstract class Day1Solution : SolutionBase
+{
+    protected List<int> Column1 { get; } = [];
+    protected List<int> Column2 { get; } = [];
+
+    public override void ReadFile()
+    {
+        this.Column1.Clear();
+        this.Column2.Clear();
+
+        using var parser = new TextFieldParser(Input);
+        parser.SetDelimiters("  ");
+        while (!parser.EndOfData)
+        {
+            var inputs = parser.ReadFields();
+            if (inputs == null) break;
+            this.Column1.Add(int.Parse(inputs[0]));
+            this.Column2.Add(int.Parse(inputs[1]));
+        }
+        
+        this.Column1.Sort();
+        this.Column2.Sort();
+    }
+}
+
+[DisplayName("Day 1/Part 1 Solution")]
+internal class Day1Part1Solution : Day1Solution
+{
+    public override void CalculateSolution()
+    {
+        this.ReadFile();
+
+        var results = new List<int>(); 
+        for (var i = 0; i < this.Column1.Count; i++)
+        {
+            results.Add(int.Abs(this.Column1[i] - this.Column2[i]));
+        }
+        var sum = results.Sum();
+        
+        RenderResult(null, sum);
+    }
+}
+
+[DisplayName("Day 1/Part 2 Solution")]
+internal class Day1Part2Solution : Day1Solution
+{
+    public override void CalculateSolution()
+    {
+        this.ReadFile();
+
+        var results = new List<int>();
+        foreach (var t in this.Column1)
+        {
+            results.Add(t * this.Column2.Count(x => x == t));
+        }
+        var sum = results.Sum();
+
+        RenderResult(null, sum);
+    }
+} 
